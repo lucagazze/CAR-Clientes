542b3c8104672b245725b561968be3bc392fedc7
lucagazze <lucagazze1@gmail.com>
2026-05-31 11:37:12 -0300

    fix: hide AIChatFloat on /atencion page

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -126,7 +126,7 @@ export const MainLayout = () => {
             </Routes>
           </Suspense>
         </div>
-        <AIChatFloat />
+        {location.pathname !== '/atencion' && <AIChatFloat />}
       </main>
     </div>
   );
